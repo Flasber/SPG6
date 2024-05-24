77e52a00ee47c48026a09dd798244fa3d5f9ef45
Carmen Mihut <mihutcarmenioana@gmail.com>
2024-05-24 20:39:02 +0200

    n

--- SPG6vestbjerg/src/model/Product.java
+++ SPG6vestbjerg/src/model/Product.java
@@ -1,25 +1,49 @@
 package model;
 
-import java.math.BigDecimal;
-
 public class Product {
- private String description;
- private String name;
- private String sku;
- private String barcode;
- private BigDecimal price;
+	private String description;
+	private String name;
+	private String sku;
+	private String barcode;
 
+	public Product(String description, String name, String sku, String barcode) {
+		this.description = description;
+		this.name = name;
+		this.sku = sku;
+		this.barcode = barcode;
 
- public Product(String description, String name, String sku, String barcode,BigDecimal price){
-    this.description=description;
-    this.name=name;
-    this.sku=sku;
-    this.barcode=barcode;
-    this.price=price;
- }
+	}
 
- public String getBarcode(){
-    return barcode;
- }
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
 
 }
--- SPG6vestbjerg/src/model/WarrantyProduct.java
+++ SPG6vestbjerg/src/model/WarrantyProduct.java
@@ -1,5 +1,19 @@
 package model;
 
-public class WarrantyProduct {
+import java.util.ArrayList;
+import java.util.List;
+
+public class WarrantyProduct extends Product {
+
+	private List<Copy> copies;
+
+	public WarrantyProduct(String description, String name, String sku, String barcode) {
+		super(description, name, sku, barcode);
+		copies = new ArrayList<>();
+	}
+
+	public List<Copy> getCopies() {
+		return copies;
+	}
 
 }
