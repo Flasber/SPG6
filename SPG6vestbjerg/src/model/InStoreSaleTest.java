e6016d6180baf61f312bd855ac5a02e1320d0438
Lærke <laerke@fastmail.com>
2024-05-24 12:55:02 +0200

    Add JUnit, add InStoreSaleTest stubs

---
+++ SPG6vestbjerg/src/model/InStoreSaleTest.java
@@ -0,0 +1,54 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InStoreSaleTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testInStoreSale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCustomer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddProductProductInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddProductProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddCopy() {
+		fail("Not yet implemented");
+	}
+
+}
