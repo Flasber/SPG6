f30114006dc756d2d7b0a39da8fd143240171ae7
Flasber <149151047+Flasber@users.noreply.github.com>
2024-05-27 09:45:32 +0200

    Product
    product.

--- SPG6vestbjerg/src/model/Product.java
+++ SPG6vestbjerg/src/model/Product.java
@@ -1,3 +1,23 @@
+import java.math.BigDecimal;
+
+public class Product {
+
+	private String description;
+	private String name;
+	private String sku;
+	private String barcode;
+	private BigDecimal price;
+
+	public Product(String description, String name, String sku, String barcode) {
+		super();
+		this.description = description;
+		this.name = name;
+		this.sku = sku;
+		this.barcode = barcode;
+
+	}
+
+	public String getName() {
 		return name;
 	}
 
