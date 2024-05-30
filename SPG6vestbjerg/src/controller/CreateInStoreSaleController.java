3068b2d30bf4a8d78726acc363367f0c3f985d3a
sarsum33 <sar.sum33@gmail.com>
2024-05-30 13:20:19 +0200

    stuff

--- SPG6vestbjerg/src/controller/CreateInStoreSaleController.java
+++ SPG6vestbjerg/src/controller/CreateInStoreSaleController.java
@@ -59,7 +59,7 @@ public class CreateInStoreSaleController {
 		return saleInProgress;
 	}
 
-	private void updateStockArfterSale() throws Exception {
+	public void updateStockArfterSale() throws Exception {
 		if (saleInProgress != null) {
 			BillableItemController bictrl = new BillableItemController();
 			for (BillableLine line : saleInProgress.getBillableLines()) {
